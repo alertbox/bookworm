9c6135eb7553d705090694d3b4728accd8730566
kosalanuwan <kosalanuwan@users.noreply.github.com>
2020-07-19 09:46:49 +0000

    deploy: bf013c04d5533eb46c126a0434f40c428bbed7f0

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/bookworm/assets/css/main.css?202007190945",
+        "/bookworm/assets/css/main.css?202007190946",
         "/bookworm/"
       ]);
     })
